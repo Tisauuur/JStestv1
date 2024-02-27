e0acec0f95cf9c1a81a868a81e5504c1028b6169
tisauuur <128767187+Tisauuur@users.noreply.github.com>
2024-02-26 21:12:08 -0400

    add getPlayerChoice and playRound functions.
    Functions were added to ask the user input and play a round of the game
    evaluating the result and displaying it in the console.

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 function getComputerChoice() {
-    const maxNum = Math.ceil(4);
-    const minNum = Math.floor(1);
-    const randomNum = Math.floor(Math.random() * (maxNum - minNum) + minNum);
-    if (randomNum === 1) {
-        console.log('Rock!')
-    }
-    else if (randomNum === 2) {
-        console.log('Paper!')
-    }
 
-    else {
-        console.log('Scissors!')
-    }
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    const computerChoice = choices[randomIndex];
+
+    return computerChoice;
 };
 
-getComputerChoice()
+function getPlayerChoice() {
+    let input = prompt("Rock, Paper or Scissors?: ");
+    let choiseLower = input.toLowerCase();
+    let = result = '';
+
+    (choiseLower === 'rock') ? result = 'rock' :
+        (choiseLower === 'paper') ? result = "papel" :
+            (choiseLower === 'scissors') ? result = 'scissors' :
+                result = 'Wrong input';
+
+    return result;
+};
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    const playerWin = `${playerSelection} is better than ${computerSelection}, you won!`;
+    const computerWin = `${computerSelection} is bettet than ${playerSelection}, you lost...`;
+    const tie = `You and the computer chosed ${playerSelection}, it's a tie!`;
+
+    if (playerSelection === computerSelection) {
+        console.log(tie)
+    }
+
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) { console.log(playerWin) }
+
+    else if (
+        (computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'paper' && playerSelection === 'rock') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper')
+    ) { console.log(computerWin) }
+
+    else {
+        console.log('Wrong input')
+    }
+
+};
+
+playRound(playerSelection, computerSelection);
